6565493954b25deed5074394479d59230ff815fc
tanutarou <tanutarou@gmail.com>
2015-05-24 00:46:44 +0900

    スクリプトの修正

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 ln -s ~/dotfiles/_vimrc ~/.vimrc
-ln -s ~/dotfiles/_vim ~/.vim
+
+# NeoBundle
+mkdir -p ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+# colorscheme
+mkdir -p ~/.vim/colors
+git clone https://github.com/tomasr/molokai
+mv ./molokai/colors/molokai.vim ~/.vim/colors
+rm -rf ./molokai
+
