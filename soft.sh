4f958bc823c269a6f30cd928fab431fd034d0c54
tanutarou <tanutarou@gmail.com>
2016-04-13 15:34:37 +0900

    Add settings for latex

---
+++ soft.sh
@@ -0,0 +1,8 @@
+# tex
+#http://qiita.com/ynakayama/items/706ae9e59c1b6fd3e3d2
+sudo apt-get install texlive-full
+sudo apt-get install texlive-lang-cjk
+sudo apt-get install xdvik-ja
+sudo apt-get install dvipsk-ja
+sudo apt-get install gv
+sudo apt-get install texlive-fonts-recommended texlive-fonts-extra
